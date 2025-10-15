d61f5049223794da57eab1105214adc253c64f24
Harper Rhett <harperrhett@gmail.com>
2025-10-14 19:09:58 -0500

    ehhh let's make these more concise

--- HarpEngine/Utilities/FloatExtensions.cs
+++ HarpEngine/Utilities/FloatExtensions.cs
@@ -2,17 +2,17 @@
 
 public static class FloatExtensions
 {
-	public static int GetRounded(this float value)
+	public static int Rounded(this float value)
 	{
 		return (int)float.Round(value);
 	}
 
-	public static int GetFloored(this float value)
+	public static int Floored(this float value)
 	{
 		return (int)float.Floor(value);
 	}
 
-	public static int GetCeiled(this float value)
+	public static int Ceiled(this float value)
 	{
 		return (int)float.Ceiling(value);
 	}
