48ce53dde896c07163369b5d840442e8b417d505
Harper Rhett <harperrhett@gmail.com>
2025-10-20 11:36:35 -0500

    New image bindings

--- HarpEngine/Graphics/Image.cs
+++ HarpEngine/Graphics/Image.cs
@@ -9,36 +9,33 @@ public unsafe partial struct Image : IDisposable
 	public int Mipmaps;
 	public PixelFormat Format;
 
-	[DllImport("raylib.dll", CallingConvention = CallingConvention.Cdecl)]
-	private static extern Image LoadImage(string fileName);
-	public static Image Load(string fileName) => LoadImage(fileName);
+	[DllImport("raylib.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "LoadImage")]
+	public static extern Image Load(string fileName);
 
-	[DllImport("raylib.dll", CallingConvention = CallingConvention.Cdecl)]
-	private static extern Image LoadImageRaw(string fileName, int width, int height, PixelFormat pixelFormat, int headerSize);
-	public static Image LoadRaw(string fileName, int width, int height, PixelFormat pixelFormat, int headerSize) => LoadImageRaw(fileName, width, height, pixelFormat, headerSize);
+	[DllImport("raylib.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "LoadImageRaw")]
+	public static extern Image LoadRaw(string fileName, int width, int height, PixelFormat pixelFormat, int headerSize);
 
-	[DllImport("raylib.dll", CallingConvention = CallingConvention.Cdecl)]
-	private static extern Image LoadImageFromTexture(Texture texture);
-	public static Image LoadFromTexture(Texture texture) => LoadImageFromTexture(texture);
+	[DllImport("raylib.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "LoadImageFromTexture")]
+	public static extern Image Load(Texture texture);
 
-	[DllImport("raylib.dll", CallingConvention = CallingConvention.Cdecl)]
+	[DllImport("raylib.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "IsImageValid")]
 	[return: MarshalAs(UnmanagedType.I1)]
-	private static extern bool IsImageValid(Image image);
-	public bool IsValid => IsImageValid(this);
+	private static extern bool IsThisValid(Image image);
+	public bool IsValid => IsThisValid(this);
 
-	[DllImport("raylib.dll", CallingConvention = CallingConvention.Cdecl)]
-	private static extern void UnloadImage(Image image);
+	[DllImport("raylib.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "UnloadImage")]
+	private static extern void Unload(Image image);
 
-	[DllImport("raylib.dll", CallingConvention = CallingConvention.Cdecl)]
+	[DllImport("raylib.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ExportImage")]
 	[return: MarshalAs(UnmanagedType.I1)]
-	private static extern bool ExportImage(Image image, string fileName);
+	private static extern bool Export(Image image, string fileName);
 	public void Export(string fileName, out bool success)
 	{
-		success = ExportImage(this, fileName);
+		success = Export(this, fileName);
 	}
 
 	public void Dispose()
 	{
-		UnloadImage(this);
+		Unload(this);
 	}
 }
