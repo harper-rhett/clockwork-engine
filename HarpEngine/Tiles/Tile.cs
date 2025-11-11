83c8e85fba134db051d9b1a5d0972c55113d78c6
Harper Rhett <harperrhett@gmail.com>
2025-11-11 16:59:48 -0600

    tiled areas

---
+++ HarpEngine/Tiles/Tile.cs
@@ -0,0 +1,23 @@
+﻿namespace HarpEngine.Tiles;
+
+public class Tile<TileType> where TileType : Enum
+{
+	private Texture tilesetTexture;
+	private Rectangle spriteRectangle;
+	public readonly TileType Type;
+
+	public Tile(Texture tilesetTexture, int tilesetX, int tilesetY, int size, TileType type, bool xFlipped, bool yFlipped)
+	{
+		this.tilesetTexture = tilesetTexture;
+		Type = type;
+
+		int width = xFlipped ? -size : size;
+		int height = yFlipped ? -size : size;
+		spriteRectangle = new(tilesetX, tilesetY, width, height);
+	}
+
+	public void Draw(Vector2 position)
+	{
+		tilesetTexture.Draw(spriteRectangle, position, Colors.White);
+	}
+}
---
+++ HarpEngine/Tiles/TiledArea.cs
@@ -0,0 +1,67 @@
+﻿namespace HarpEngine.Tiles;
+
+public class TiledArea<TileType> : Entity where TileType : Enum
+{
+	public readonly Vector2 Position;
+	public readonly int WidthInTiles;
+	public readonly int HeightInTiles;
+	public readonly int WidthInPixels;
+	public readonly int HeightInPixels;
+	public readonly int TileSize;
+	private readonly RenderTexture renderTexture;
+	private readonly Rectangle renderRectangle;
+	private Tile<TileType>[,] tiles;
+
+	public TiledArea(Scene scene, Vector2 position, int widthInTiles, int heightInTiles, int tileSize, Tile<TileType>[,] tiles) : base(scene)
+	{
+		Position = position;
+		WidthInTiles = widthInTiles;
+		HeightInTiles = heightInTiles;
+		WidthInPixels = widthInTiles * tileSize;
+		HeightInPixels = heightInTiles * tileSize;
+		TileSize = tileSize;
+		renderTexture = RenderTexture.Load(WidthInPixels, HeightInPixels);
+		renderRectangle = new(0, 0, WidthInPixels, -HeightInPixels);
+
+		ProcessTexture();
+	}
+
+	private void ProcessTexture()
+	{
+		RenderTexture.BeginDrawing(renderTexture);
+		for (int tileX = 0; tileX < WidthInTiles; tileX++)
+			for (int tileY = 0; tileY < HeightInTiles; tileY++)
+			{
+				Tile<TileType> tile = tiles[tileX, tileY];
+				Vector2 tilePosition = Position + new Vector2(tileX * TileSize, tileY * TileSize);
+				tile.Draw(tilePosition);
+			}
+		RenderTexture.EndDrawing();
+	}
+
+	public override void Draw()
+	{
+		renderTexture.Texture.Draw(renderRectangle, Position, Colors.White);
+	}
+
+	public TileType GetTile(int pixelX, int pixelY)
+	{
+		int tileX = (pixelX - Position.X.Floored()) / TileSize;
+		int tileY = (pixelY - Position.Y.Floored()) / TileSize;
+		return tiles[tileX, tileY].Type;
+	}
+
+	public bool InBounds(int pixelX, int pixelY)
+	{
+		bool xCheck = pixelX >= Position.X.Floored() && pixelX < Position.X.Floored() + WidthInPixels;
+		bool yCheck = pixelY >= Position.Y.Floored() && pixelY < Position.Y.Floored() + HeightInPixels;
+		return xCheck && yCheck;
+	}
+
+	public Vector2 SnapPosition(int pixelX, int pixelY)
+	{
+		int tilePixelX = ((float)pixelX / TileSize).Floored() * TileSize;
+		int tilePixelY = ((float)pixelY / TileSize).Floored() * TileSize;
+		return new(tilePixelX, tilePixelY);
+	}
+}
