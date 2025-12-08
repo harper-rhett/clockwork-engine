ab77ef2c58de0d6c89cb443d42fd96b031417dd2
Harper Rhett <harperrhett@gmail.com>
2025-12-08 09:09:23 -0600

    started on parallax

---
+++ HarpEngine/Graphics/Parallax.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace HarpEngine.Graphics;
+
+public class Parallax : Entity
+{
+	private Camera2D camera;
+	private List<Layer> layers = new();
+
+	public Parallax(Camera2D camera)
+	{
+		this.camera = camera;
+	}
+
+	public void AddLayer(Texture backgroundTexture, Vector2 offset, float speed)
+	{
+		layers.Add(new(backgroundTexture, offset, speed));
+	}
+
+	public override void OnDraw()
+	{
+		foreach (Layer layer in layers)
+		{
+			Vector2 position = camera.Transform.WorldPosition * -layer.Speed + layer.Offset;
+			layer.Texture.Draw(position, Colors.White);
+			Console.WriteLine(position);
+		}
+	}
+
+	public class Layer
+	{
+		public readonly Texture Texture;
+		public readonly Vector2 Offset;
+		public readonly float Speed;
+
+		public Layer(Texture texture, Vector2 offset, float speed)
+		{
+			Texture = texture;
+			Offset = offset;
+			Speed = speed;
+		}
+	}
+}
