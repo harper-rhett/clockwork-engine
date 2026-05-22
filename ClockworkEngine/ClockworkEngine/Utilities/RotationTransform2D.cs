7b1d515a6fa06160910335bee54c1b9a05261c9a
Harper Rhett <harperrhett@gmail.com>
2026-05-22 06:23:44 -0400

    new transforms

---
+++ ClockworkEngine/ClockworkEngine/Utilities/RotationTransform2D.cs
@@ -0,0 +1,35 @@
+﻿using System.Numerics;
+
+namespace Clockwork.Utilities;
+
+public class RotationTransform2D
+{
+	public RotationTransform2D Parent;
+
+	public float WorldRotation
+	{
+		get => Parent is null ? LocalRotation : Parent.WorldRotation + LocalRotation;
+		set => LocalRotation = Parent is null ? value : value - Parent.WorldRotation;
+	}
+
+	public float LocalRotation;
+
+	public Matrix3x2 Matrix
+	{
+		get
+		{
+			Matrix3x2 rotationMatrix = Matrix3x2.CreateRotation(float.DegreesToRadians(LocalRotation));
+			if (Parent is null) return rotationMatrix;
+			else return rotationMatrix * Parent.Matrix;
+		}
+	}
+
+	public Matrix3x2 MatrixInverse
+	{
+		get
+		{
+			Matrix3x2.Invert(Matrix, out Matrix3x2 WorldToLocalMatrix);
+			return WorldToLocalMatrix;
+		}
+	}
+}
---
+++ ClockworkEngine/ClockworkEngine/Utilities/TranslationTransform2D.cs
@@ -0,0 +1,35 @@
+﻿using System.Numerics;
+
+namespace Clockwork.Utilities;
+
+public class TranslationTransform2D
+{
+	public TranslationTransform2D Parent;
+
+	public Vector2 WorldPosition
+	{
+		get => Parent is null ? LocalPosition : Vector2.Transform(LocalPosition, Parent.Matrix);
+		set => LocalPosition = Parent is null ? value : Vector2.Transform(value, Parent.MatrixInverse);
+	}
+
+	public Vector2 LocalPosition;
+
+	public Matrix3x2 Matrix
+	{
+		get
+		{
+			Matrix3x2 translationMatrix = Matrix3x2.CreateTranslation(LocalPosition);
+			if (Parent is null) return translationMatrix;
+			else return translationMatrix * Parent.Matrix;
+		}
+	}
+
+	public Matrix3x2 MatrixInverse
+	{
+		get
+		{
+			Matrix3x2.Invert(Matrix, out Matrix3x2 WorldToLocalMatrix);
+			return WorldToLocalMatrix;
+		}
+	}
+}
