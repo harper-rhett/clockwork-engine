64291cf4219f2227fe46124399ec327126f4567b
Harper Rhett <harperrhett@gmail.com>
2026-05-31 13:42:28 -0500

    started on a profiler

---
+++ ClockworkEngine/ClockworkEngine/PerformanceTracker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Clockwork;
+
+public class PerformanceTracker
+{
+	private Stopwatch stopwatch = new();
+	private Queue<float> millisecondsTracked = new();
+
+	public void Start()
+	{
+		stopwatch.Restart();
+	}
+
+	public void Stop()
+	{
+		stopwatch.Stop();
+		millisecondsTracked.Enqueue(stopwatch.ElapsedMilliseconds);
+		if (millisecondsTracked.Count > Profiler.FramesTracked) millisecondsTracked.Dequeue();
+	}
+
+	public float GetAverageTime()
+	{
+		return millisecondsTracked.Average();
+	}
+}
---
+++ ClockworkEngine/ClockworkEngine/Profiler.cs
@@ -0,0 +1,36 @@
+﻿using Clockwork.Graphics;
+using Clockwork.Graphics.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Clockwork;
+
+public static class Profiler
+{
+	private static Dictionary<string, PerformanceTracker> trackers = new();
+	public static int FramesTracked = 300;
+
+	public static void StartTracking(string key)
+	{
+		if (trackers.TryGetValue(key, out PerformanceTracker tracker)) tracker.Start();
+		else
+		{
+			PerformanceTracker newTracker = new();
+			newTracker.Start();
+			trackers[key] = newTracker;
+		}
+	}
+
+	public static void StopTracking(string key)
+	{
+		if (trackers.TryGetValue(key, out PerformanceTracker tracker)) tracker.Stop();
+		else throw new InvalidOperationException("Must start tracking first.");
+	}
+
+	public static void DrawDebug(int fontSize, Color color)
+	{
+		string[] logs = trackers.Keys.Select(valueName => $"{valueName}: {trackers[valueName].GetAverageTime():F2}ms").ToArray();
+		Text.DrawDebug(fontSize, color, logs);
+	}
+}
